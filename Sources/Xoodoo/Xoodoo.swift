c9f1c7b95f27f84ddb24f858fe8e746b3baa206b
nixberg <310825+nixberg@users.noreply.github.com>
2023-03-13 14:53:10 +0100

    Remove noisy, untestable big-endian support

--- Sources/Xoodoo/Xoodoo.swift
+++ Sources/Xoodoo/Xoodoo.swift
@@ -6,10 +6,6 @@ public struct Xoodoo {
     public init() {}
     
     public mutating func permute() {
-        a = SIMD4(littleEndian: a)
-        b = SIMD4(littleEndian: b)
-        c = SIMD4(littleEndian: c)
-        
         for roundConstant: UInt32 in [
             0x058, 0x038, 0x3c0, 0x0d0, 0x120, 0x014,
             0x060, 0x02c, 0x380, 0x0f0, 0x1a0, 0x012,
@@ -32,24 +28,10 @@ public struct Xoodoo {
             b = b.rotated(left: 1)
             c = c[11, 8, 9, 10, 15, 12, 13, 14, 3, 0, 1, 2, 7, 4, 5, 6]
         }
-        
-        a = a.littleEndian
-        b = b.littleEndian
-        c = c.littleEndian
     }
 }
 
 private extension SIMD4 where Scalar == UInt32 {
-    @inline(__always)
-    init(littleEndian value: Self) {
-        self.init(
-            Scalar(littleEndian: value.x),
-            Scalar(littleEndian: value.y),
-            Scalar(littleEndian: value.z),
-            Scalar(littleEndian: value.w)
-        )
-    }
-    
     @inline(__always)
     func rotatingLanes(right count: Int) -> Self {
         assert(count == 1)
@@ -65,14 +47,8 @@ private extension SIMD4 where Scalar == UInt32 {
     subscript(_ indices: UInt8...) -> Self {
         unsafeBitCast(unsafeBitCast(self, to: SIMD16<UInt8>.self)[SIMD16(indices)], to: Self.self)
     }
-    
-    @inline(__always)
-    var littleEndian: Self {
-        Self(
-            x.littleEndian,
-            y.littleEndian,
-            z.littleEndian,
-            w.littleEndian
-        )
-    }
 }
+
+#if _endian(big)
+#error("Big-endian platforms are currently not supported")
+#endif
